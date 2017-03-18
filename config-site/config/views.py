1d7498c74fc71edd52c068237c2eb0a8bbac2d45
Home Raspberry Pi <jamesfowkes@gmail.com>
2017-03-18 15:30:32 +0000

    Add lamp status to config site view

--- config-site/config/views.py
+++ config-site/config/views.py
@@ -10,14 +10,21 @@ standard_view = Blueprint('standard_view', __name__, template_folder='templates'
 
 config = PersistentConfig(app.config["SHELVE_FILENAME"])
 
+from collections import namedtuple
+
+class ApplicationView(namedtuple("ApplicationView", ["rgb"])):
+	pass
+
 @standard_view.route("/", methods=['GET', 'POST'])
 def homepage():
 
+	app_view = ApplicationView((0,0,0))
+	
 	if request.method == 'POST':
 		form = ConfigForm(request.form)
 		config.set(**form.data)
-		return render_template("standard_view.html", form=form)
+		return render_template("standard_view.html", form=form, app_view=app_view)
 
 	else:
 		form = ConfigForm(obj=config)
-		return render_template("standard_view.html", form=form)
+		return render_template("standard_view.html", form=form, app_view=app_view)
